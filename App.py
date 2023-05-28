61e7cbeed57c32438d3d99f83dd8c8ba6e5a5204
Vadim Shchepin <vadimshch21@gmail.com>
2023-05-28 11:32:37 +0200

    added custom fields for columns

--- App.py
+++ App.py
@@ -2,10 +2,14 @@ import streamlit as st
 import pandas as pd
 from rapidapi import extend_dataframe
 from article_info import show_articles_information
+from gpt_functions import ChatGPT
+
+
+chat_gpt = ChatGPT("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")
 
 st.set_page_config(
    page_title="Product Extender",
-   page_icon="🧊",
+   page_icon=":smile:",
    layout="wide",
    initial_sidebar_state="expanded",
 )
@@ -24,11 +28,13 @@ if uploaded_file is not None:
     st.session_state.uploaded_file = df
     st.dataframe(df)
 else:
-    st.write('Upload file')
+    df = pd.read_csv('inputs/output.csv', sep=',')
+    st.session_state.uploaded_file = df
+    st.write('Uploaded file not found, preset used')
 
 if st.button('Extend file'):
     with st.spinner('Wait for it...'):
-        if st.session_state.uploaded_file is not None:
+        if  st.session_state.uploaded_file is not None:
             st.session_state.extended_df = extend_dataframe(st.session_state.uploaded_file)  
             st.dataframe(st.session_state.extended_df[["EAN","artikelordernummer","Artikelname","product_description", "product_photos", "product_attributes"]])
             st.success('Done!')
@@ -40,11 +46,32 @@ if st.button('Extend file'):
 else:
     st.write('File is not extended')
 
-if st.session_state.extended_df is not None:
-    if st.button('Extend file with chatgpt'):
-        
-else: st.write('you must download file')
-# inputdf = pd.read_csv('inputs/output.csv', sep=',', skipinitialspace=True)
-# st.dataframe(inputdf)
+# if st.session_state.extended_df is not None:
+# else: st.write('you must download file')
 
 # show_articles_information(inputdf)
+cols = st.columns([1,2])
+with cols[0].container():
+    inputtxt = st.text_area('User Input')
+with cols[1].container():
+    systeminputtxt = st.text_area('System Input')
+if st.button('Ask chat GPT-3.5'):
+    with st.spinner('Wait for it...'):
+        st.write(chat_gpt.make_response(systeminputtxt, inputtxt))
+
+if st.session_state.uploaded_file is not None:
+    options = st.multiselect('Which columns should be analyzed?', st.session_state.uploaded_file.columns)
+for option in options:
+    cols = st.columns([0.5,2,1])
+    with cols[0].container():
+        st.write(option);
+    with cols[1].container():
+        inputtxt = st.text_area('User Input for ' + option)
+    with cols[2].container():
+        systeminputtxt = st.text_area('System Input for ' + option)
+    if st.button('Ask chat GPT-3.5'):
+        with st.spinner('Wait for it...'):
+            st.write(chat_gpt.make_response(systeminputtxt + " " + option, inputtxt))
+
+        
+    st.divider()
