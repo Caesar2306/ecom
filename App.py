9ec40c6107b5f31654d575d67e8c846fceaef837
Vadim Shchepin <vadimshch21@gmail.com>
2023-05-29 14:26:35 +0200

    gpt rows iteration added, for selected column, small test dataset added

--- App.py
+++ App.py
@@ -3,6 +3,8 @@ import pandas as pd
 from rapidapi import extend_dataframe
 from article_info import show_articles_information
 from gpt_functions import ChatGPT
+import openai 
+import time
 
 
 chat_gpt = ChatGPT("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")
@@ -23,12 +25,13 @@ if 'extended_df' not in st.session_state:
     st.session_state.extended_df = None
 
 uploaded_file = st.file_uploader("Choose a file")
+
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file, sep=';')
     st.session_state.uploaded_file = df
     st.dataframe(df)
 else:
-    df = pd.read_csv('inputs/output.csv', sep=',')
+    df = pd.read_csv('inputs/output_short.csv', sep=',')
     st.session_state.uploaded_file = df
     st.write('Uploaded file not found, preset used')
 
@@ -41,7 +44,6 @@ if st.button('Extend file'):
     
     if st.session_state.extended_df is not None:
         show_articles_information(st.session_state.extended_df)
-
         st.download_button('Download file', st.session_state.extended_df.to_csv(index=False), file_name='output.csv')
 else:
     st.write('File is not extended')
@@ -49,27 +51,62 @@ else:
 # if st.session_state.extended_df is not None:
 # else: st.write('you must download file')
 
+tab1, tab2, tab3 = st.tabs(["options", ":chat: Chat", "🗃 Data"])
+
+tab1.subheader("options")
 # show_articles_information(inputdf)
-cols = st.columns([1,2])
-with cols[0].container():
-    inputtxt = st.text_area('User Input')
-with cols[1].container():
-    systeminputtxt = st.text_area('System Input')
-if st.button('Ask chat GPT-3.5'):
-    with st.spinner('Wait for it...'):
-        st.write(chat_gpt.make_response(systeminputtxt, inputtxt))
+tab2.write("tab2")
+
+tab2.subheader(":chat: Chat")
+with tab2.container():
+    cols = st.columns([1,2])
+    with cols[0].container():
+        inputtxt = st.text_area('User Input')
+    with cols[1].container():
+        systeminputtxt = st.text_area('System Input')
+    if st.button('Ask chat GPT-3.5'):
+        with st.spinner('Wait for it...'):
+            st.write(chat_gpt.make_response(systeminputtxt, inputtxt))
+
+
+tab3.subheader("🗃 Data")
+tab3.write("tab3")
+
+
 
 if st.session_state.uploaded_file is not None:
     options = st.multiselect('Which columns should be analyzed?', st.session_state.uploaded_file.columns)
 for option in options:
     cols = st.columns([0.5,2,1])
     with cols[0].container():
-        st.write(option);
+        st.write(option)
     with cols[1].container():
-        inputtxt = st.text_area('User Input for ' + option)
+        inputtxt = st.text_area('User input (option information fetched automatically)' + option)
     with cols[2].container():
-        systeminputtxt = st.text_area('System Input for ' + option)
-    if st.button('Ask chat GPT-3.5'):
+        systeminputtxt = st.text_area('System (Product name added automatically) ' + option)
+    if st.button('Ask chat GPT-3.5 to update ' + option):
         with st.spinner('Wait for it...'):
-            st.write(chat_gpt.make_response(systeminputtxt + " " + option, inputtxt))
+            new_column = []
+            for index, row in st.session_state.uploaded_file.iterrows():
+                success=False
+                ean = row['EAN']
+                while not success:
+                    try:
+                        extended_value = chat_gpt.make_response(systeminputtxt + " for the product " + str(row['Artikelname']), inputtxt + ' ' + str(row[option]))
+                        new_column.append(extended_value)
+                        st.write('trying '+str(ean))
+                        success=True
+                    except openai.error.RateLimitError as e:
+                        print(f"Rate limit error: {e}. Retrying in 5 seconds...")
+                        time.sleep(5)
+                    except openai.error.APIError as e:
+                        print(f"API error: {e}. Retrying in 5 seconds...")
+                        time.sleep(5)
+                    except Exception as e:
+                        print(f"Unexpected error: {e}. Retrying in 5 seconds...")
+                        time.sleep(5)
+                        print(f"No data returned from API for product with EAN {ean}.")
+            st.session_state.uploaded_file[option+"_extended"] = new_column
+            with st.spinner('Waiting for updates'):
+                st.dataframe(st.session_state.uploaded_file[['EAN',option,option+'_extended']])
     st.divider()
