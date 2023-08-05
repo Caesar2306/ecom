72d10907af821f963c5cfd9976b994c82d7559f6
Vadim Shchepin <vadimshch21@gmail.com>
2023-08-05 15:06:41 +0200

    added new functionality, tested on local system

--- App.py
+++ App.py
@@ -114,23 +114,35 @@ st.set_page_config(
 #     st.divider()
 
 # URL and credentials
-url = "https://schoenheitsberatung.de/euphorika-dev-2021/api/articles"
-api_key = "NtFc6aIB9kyktckuRqb4mTkkcC8iVvxNSHLSaf4F"
+url = "http://localhost:8000/api/articles"
+api_key = "IbqzBaJMnif6qRwXGa4smXfWcsVvcn8iQDMiWgwF"
+# api_key = "NtFc6aIB9kyktckuRqb4mTkkcC8iVvxNSHLSaf4F"
 backend_username = "vadim"
+st.subheader('Get Products')
 
 # Available MySQL expressions
-mysql_expressions = ["LIKE", "=", ">=", "<=", ">", "<"]
+mysql_expressions = ["LIKE", "=", ">=", "<=", ">", "<", "IN"]
 
+# Available columns
+columns = [
+    "id", "mainDetailId", "supplierId", "taxId", "priceGroupId", "filterGroupId",
+    "configuratorSetId", "name", "description", "descriptionLong", "added", "active",
+    "pseudoSales", "highlight", "keywords", "metaTitle", "changed", "priceGroupActive",
+    "lastStock", "crossBundleLook", "notification", "template", "mode", "availableFrom",
+    "availableTo", "mainDetail"
+]
+default_columns = ["id", "mainDetailId", "supplierId", "name", "description", "active"]
 # Authentication function
 def get_auth():
     return HTTPBasicAuth(backend_username, api_key)
 
 # Get articles function with filter, sort, limit, and offset
 def get_all_articles(selected_columns, filters, sorts, limit, offset):
-    params = {
-        "filter": filters,
-        "sort": sorts
-    }
+    params = {}
+    if filters:
+        params["filter"] = filters
+    if sorts:
+        params["sort"] = sorts
     if limit:
         params["limit"] = limit
     if offset:
@@ -143,50 +155,101 @@ def get_all_articles(selected_columns, filters, sorts, limit, offset):
         return filtered_articles
     else:
         return None
-# Available columns
-columns = [
-    "id", "mainDetailId", "supplierId", "taxId", "priceGroupId", "filterGroupId",
-    "configuratorSetId", "name", "description", "descriptionLong", "added", "active",
-    "pseudoSales", "highlight", "keywords", "metaTitle", "changed", "priceGroupActive",
-    "lastStock", "crossBundleLook", "notification", "template", "mode", "availableFrom",
-    "availableTo", "mainDetail"
+# List of available fields
+available_fields = [
+    "id", "active", "added", "changed", "crossBundleLook", "description", 
+    "descriptionLong", "filterGroupId", "highlight", "keywords", 
+    "lastStock", "metaTitle", "mode", "name", "notification", "priceGroupActive",
+    "pseudoSales", "mainDetailId", "supplierId", "taxId", "priceGroupId",
+    "configuratorSetId", "categories", "customerGroups", "details", "downloads",
+    "images", "links", "mainDetail", "propertyGroup", "propertyValues",
+    "seoCategories", "similar", "supplier", "tax", "template"
 ]
-default_columns = ["id", "mainDetailId", "supplierId", "name", "description", "active"]
+
 # Multiselect box for columns
 selected_columns = st.multiselect("Select Columns", options=columns, default=default_columns)
 
+# Function to extract selected fields from article data
+def extract_article_data(article, selected_fields):
+    extracted_data = {}
+    for field in selected_fields:
+        if field in article:
+            extracted_data[field] = article[field]
+        else:
+            # Check for nested fields
+            for key, value in article.items():
+                if isinstance(value, dict) and field in value:
+                    extracted_data[field] = value[field]
+    return extracted_data
+
+# Function to get articles based on order numbers
+def get_articles_by_order_numbers(order_numbers, filters=None, sort=None, limit=None):
+    articles = []
+    for number in order_numbers:
+        params = {"useNumberAsId": "true"}
+        if filters:
+            params["filter"] = filters
+        if sort:
+            params["sort"] = sort
+        if limit:
+            params.update(limit)
+
+        response = requests.get(f"{url}/{number}", auth=get_auth(), params=params)
+        if response.status_code == 200:
+            articles.append(response.json())
+        else:
+            st.warning(f"Failed to retrieve article with order number {number}. Status code: {response.status_code}")
+
+    return articles
+
+
+
 # Filter options
 filters = []
-st.write("Filters:")
-for i in range(3):  # Allowing up to 3 filter conditions
-    property_filter = st.selectbox(f"Filter Property {i+1}", options=columns, index=0, key=f"property{i}")
-    expression_filter = st.selectbox(f"Expression {i+1}", options=mysql_expressions, index=0, key=f"expression{i}")
-    value_filter = st.text_input(f"Value {i+1}", key=f"value{i}")
-    operator_filter = st.selectbox(f"Operator {i+1} (AND: AND, OR: OR)", options=['AND', 'OR'], index=0, key=f"operator{i}")
-    if property_filter and value_filter:
-        filters.append({
-            "property": property_filter,
-            "expression": expression_filter,
-            "value": value_filter,
-            "operator": operator_filter
-        })
+use_filter = st.checkbox("Use Filter")
+if use_filter:
+    st.write("Filters:")
+    filter_count = st.number_input("Number of Filters:", min_value=1, max_value=5, value=1, step=1)
+    for i in range(filter_count):
+        cols = st.columns(4)
+        property_filter = cols[0].selectbox(f"Property {i+1}", options=columns, index=0, key=f"property{i}")
+        expression_filter = cols[1].selectbox(f"Expression {i+1}", options=mysql_expressions, index=0, key=f"expression{i}")
+        value_filter = cols[2].text_input(f"Value {i+1}", key=f"value{i}")
+        operator_filter = cols[3].selectbox(f"Operator {i+1} (0: AND, 1: OR)", options=[0, 1], index=0, key=f"operator{i}")
+        use_operator = cols[3].checkbox("Use Operator", key=f"use_operator{i}")
+        if property_filter and value_filter:
+            filter_dict = {
+                "property": property_filter,
+                "expression": expression_filter,
+                "value": value_filter,
+            }
+            if use_operator:
+                filter_dict["operator"] = operator_filter
+            filters.append(filter_dict)
 
 # Sort options
 sorts = []
-st.write("Sorts:")
-for i in range(3):  # Allowing up to 3 sort conditions
-    property_sort = st.selectbox(f"Sort Property {i+1}", options=columns, index=0, key=f"sort_property{i}")
-    direction_sort = st.selectbox(f"Direction {i+1}", options=["ASC", "DESC"], index=0, key=f"direction{i}")
-    if property_sort:
-        sorts.append({
-            "property": property_sort,
-            "direction": direction_sort
-        })
+use_sort = st.checkbox("Use Sort")
+if use_sort:
+    st.write("Sorts:")
+    sort_count = st.number_input("Number of Sorts:", min_value=1, max_value=5, value=1, step=1)
+    for i in range(sort_count):
+        cols = st.columns(2)
+        property_sort = cols[0].selectbox(f"Sort Property {i+1}", options=columns, index=0, key=f"sort_property{i}")
+        direction_sort = cols[1].selectbox(f"Direction {i+1}", options=["ASC", "DESC"], index=0, key=f"direction{i}")
+        if property_sort:
+            sorts.append({
+                "property": property_sort,
+                "direction": direction_sort
+            })
 
 # Limit and Offset options
-limit = st.number_input("Limit:", min_value=1, max_value=1000, value=1000, step=1)
-offset = st.number_input("Offset:", min_value=0, max_value=999, value=0, step=1)
-
+use_limit_offset = st.checkbox("Use Limit/Offset")
+limit = None
+offset = None
+if use_limit_offset:
+    limit = st.number_input("Limit:", min_value=1, max_value=1000, value=1000, step=1)
+    offset = st.number_input("Offset:", min_value=0, max_value=999, value=0, step=1)
 # Button to trigger API request
 if st.button("Get Articles") and selected_columns:
     articles = get_all_articles(selected_columns, filters, sorts, limit, offset)
@@ -198,3 +261,31 @@ if st.button("Get Articles") and selected_columns:
         st.write("Failed to retrieve articles.")
 elif not selected_columns:
     st.write("Please select at least one column.")
+st.subheader('Find product using Ordernumber')
+# Multi-select input in Streamlit
+selected_fields = st.multiselect(
+    "Choose the fields you want to display",
+    options=available_fields,
+    default=["id", "name", "description", "active", "mainDetail"]
+)
+order_numbers_input = st.text_input("Enter Article Order Numbers (comma separated):")
+if order_numbers_input:
+    order_numbers = [number.strip() for number in order_numbers_input.split(",")]
+
+    # Retrieve articles based on order numbers
+    articles = get_articles_by_order_numbers(order_numbers, filters=filters)  # Add sort and limit if needed
+
+    # Extract only the selected fields
+    articles_selected_fields = [extract_article_data(article, selected_fields) for article in articles]
+
+    # Display articles
+    if articles_selected_fields:
+        st.write("Displaying articles:")
+        for article in articles_selected_fields:
+            st.write(f"Name: {article['name']}")
+            st.write(f"Main Detail Number: {article['mainDetail']['number']}")
+            st.json(article,expanded=False)
+    else:
+        st.write("Failed to retrieve articles.")
+
+st.subheader('Delete Products')
